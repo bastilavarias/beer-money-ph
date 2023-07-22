fb4d1e157097bf060360ac416d65459a58affc91
Sebastian <sebastiancurtislavarias@gmail.com>
2023-07-22 22:26:55 +0800

    Added banner

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,14 +7,28 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import SportsBarIcon from "@mui/icons-material/SportsBar";
 import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 
 export default function MainLayout({ children }) {
-    const theme = useTheme();
     const style = {
         root: {
-            backgroundColor: theme.palette.primary.main,
+            backgroundColor: "f9f9f9",
             height: "100vh",
             color: "white",
+        },
+        banner: {
+            position: "relative",
+            width: "100%",
+            height: "25vh",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+        },
+        bannerOverlay: {
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            opacity: ".8",
         },
     };
 
@@ -57,6 +70,20 @@ export default function MainLayout({ children }) {
                         </Toolbar>
                     </Container>
                 </AppBar>
+                <Container>
+                    <Box
+                        style={{
+                            ...style.banner,
+                            backgroundImage:
+                                'url("storage/images/home-banner.jpg")',
+                        }}
+                    >
+                        <Paper
+                            elevation={0}
+                            style={style.bannerOverlay}
+                        ></Paper>
+                    </Box>
+                </Container>
 
                 <main>{children}</main>
             </Box>
