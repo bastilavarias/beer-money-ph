a1b67fe768ce44265186496fd17a821e97943ec1
Sebastian Curtis Lavarias <slavarias@chanzglobal.com>
2023-08-02 17:02:25 +0800

    Job information modal

--- resources/js/Components/JobInformationModal.jsx
+++ resources/js/Components/JobInformationModal.jsx
@@ -5,17 +5,59 @@ import Modal from "@mui/material/Modal";
 import { useTheme } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Chip from "@mui/material/Chip";
 import Button from "@mui/material/Button";
 
 export default function JobInformationModal({ open, rootRef, setIsModalOpen }) {
     const [openLocal, setOpenLocal] = React.useState(open);
 
     const theme = useTheme();
+    console.log(theme);
 
-    const handleModalOpen = () => setOpenLocal(open);
-    const onHandleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+    const onHandleCloseModal = () => setIsModalOpen(false);
+
+    const JobDetailsListItem = ({ title, subtitle }) => (
+        <ListItem>
+            <ListItemText
+                primary={
+                    <React.Fragment>
+                        <Typography
+                            component="h6"
+                            typography="caption"
+                            sx={{
+                                fontWeight: 800,
+                                color: theme.palette.grey[700],
+                                textTransform: "uppercase",
+                            }}
+                        >
+                            {title}
+                        </Typography>
+                    </React.Fragment>
+                }
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            component="p"
+                            typography="subtitle2"
+                            sx={{
+                                fontWeight: 600,
+                                color: "black",
+                            }}
+                        >
+                            {subtitle}
+                        </Typography>
+                    </React.Fragment>
+                }
+            />
+        </ListItem>
+    );
 
     return (
         <Modal
@@ -45,27 +87,43 @@ export default function JobInformationModal({ open, rootRef, setIsModalOpen }) {
                     position: "relative",
                 }}
             >
-                <Button onClick={onHandleCloseModal}>Close modal</Button>
                 <Grid container>
-                    <Grid item xs={12} paddingBottom={8}>
+                    <Grid item xs={12} paddingBottom={3}>
                         <Box
                             sx={{
                                 display: "flex",
-                                alignItems: "center",
+                                alignItems: "start",
                                 flexDirection: "column",
                             }}
                         >
-                            <Box>
-                                <Typography variant="h4" component="h4">
-                                    Encode human resource data
-                                </Typography>
-                            </Box>
+                            <Typography variant="h4" component="h4">
+                                Encode human resource data
+                            </Typography>
+                            <Typography
+                                variant="h6"
+                                component="h6"
+                                sx={{
+                                    color: theme.palette.success.main,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                ₱400.00 - ₱500.00
+                            </Typography>
                             <Box
                                 sx={{
                                     display: "flex",
                                     alignItems: "center",
                                     justifyContent: "center",
-                                    paddingTop: 2,
+                                    paddingBottom: 2,
+                                }}
+                            >
+                                <Chip label="Microtask" size="small" />
+                            </Box>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "start",
                                 }}
                             >
                                 <Box
@@ -91,73 +149,149 @@ export default function JobInformationModal({ open, rootRef, setIsModalOpen }) {
                                         Sebastian Curtis L.
                                     </Typography>
                                 </Box>
-                                <Box sx={{ paddingX: 1 }}>-</Box>
-                                <Box
+                            </Box>
+                            <Box sx={{ paddingTop: 4 }}>
+                                <Button
+                                    size="small"
                                     sx={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
+                                        textTransform: "capitalize",
+                                        paddingX: 5,
                                     }}
+                                    variant="contained"
+                                    color="primary"
+                                    disableElevation={true}
                                 >
-                                    <Typography
-                                        variant="subtitle1"
-                                        component="span"
-                                        sx={{
-                                            color: theme.palette.success.main,
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        ₱400.00
-                                    </Typography>
-                                </Box>
+                                    Apply
+                                </Button>
                             </Box>
                         </Box>
                     </Grid>
-                    <Grid item xs={8}>
-                        <Typography
-                            id="server-modal-description"
-                            sx={{ pt: 2 }}
-                        >
-                            🎬 Job Opportunity: Video Editor (Remote, Non-USA)
-                            <br />
-                            <br />
-                            🏢 About Us: Skyhaus Media is a leading video
-                            marketing company specializing in creating
-                            captivating content for commercial and residential
-                            real estate clients. We also collaborate with small
-                            businesses, nonprofits, and government entities,
-                            offering a wide range of services including event
-                            recaps, testimonials, interviews, and brand stories.
-                            <br />
-                            <br />
-                            🔍 Job Summary: We are seeking talented video
-                            editors to join our team for consistent video
-                            projects! The ideal candidate should have a passion
-                            for architecture and real estate, and be skilled in
-                            video editing software such as Final Cut Pro, Adobe
-                            Premiere Pro, Adobe After Effects, and preferably
-                            Adobe Audition and DaVinci Resolve. Experience with
-                            other programs is a plus. 🎯 Key Skills: Proficient
-                            in Final Cut Pro, Adobe Premiere Pro, and Adobe
-                            After Effects. Preferably experienced in Adobe
-                            Audition and DaVinci Resolve. Additional experience
-                            in color grading, audio mastering, graphic design,
-                            or animation is a bonus. 🌍 Remote Opportunity: This
-                            position is open to video editors based outside of
-                            the USA, as we aim to diversify our global team and
-                            collaborate with talents worldwide. 💼 How to Apply:
-                            If you're passionate about video editing, have a
-                            keen eye for detail, and are excited about being
-                            part of our dynamic team, we'd love to hear from
-                            you! Please send your portfolio and resume to
-                            [hiring email/contact information]. 🌟 Join us in
-                            shaping the future of video marketing and creating
-                            exceptional content that captivates audiences
-                            worldwide.
-                        </Typography>
+                    <Grid item xs={12}>
+                        <Divider />
                     </Grid>
-                    <Grid item xs={4}>
-                        More information here
+                    <Grid container item xs={12} paddingTop={3} spacing={2}>
+                        <Grid item xs={7}>
+                            <Typography
+                                id="server-modal-description"
+                                sx={{ pt: 2 }}
+                            >
+                                🎬 Job Opportunity: Video Editor (Remote,
+                                Non-USA)
+                                <br />
+                                <br />
+                                🏢 About Us: Skyhaus Media is a leading video
+                                marketing company specializing in creating
+                                captivating content for commercial and
+                                residential real estate clients. We also
+                                collaborate with small businesses, nonprofits,
+                                and government entities, offering a wide range
+                                of services including event recaps,
+                                testimonials, interviews, and brand stories.
+                                <br />
+                                <br />
+                                🔍 Job Summary: We are seeking talented video
+                                editors to join our team for consistent video
+                                projects! The ideal candidate should have a
+                                passion for architecture and real estate, and be
+                                skilled in video editing software such as Final
+                                Cut Pro, Adobe Premiere Pro, Adobe After
+                                Effects, and preferably Adobe Audition and
+                                DaVinci Resolve. Experience with other programs
+                                is a plus. 🎯 Key Skills: Proficient in Final
+                                Cut Pro, Adobe Premiere Pro, and Adobe After
+                                Effects. Preferably experienced in Adobe
+                                Audition and DaVinci Resolve. that captivates
+                                audiences worldwide.
+                            </Typography>
+                            <Box sx={{ paddingTop: 4 }}>
+                                <Button
+                                    size="small"
+                                    sx={{
+                                        textTransform: "capitalize",
+                                        paddingX: 5,
+                                    }}
+                                    variant="contained"
+                                    color="primary"
+                                    disableElevation={true}
+                                >
+                                    Apply
+                                </Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Grid item xs={12}>
+                                <Grid item xs={12} paddingBottom={2}>
+                                    <Card color="primary" variant="outlined">
+                                        <List
+                                            dense={true}
+                                            sx={{
+                                                width: "100%",
+                                                bgcolor: "background.paper",
+                                            }}
+                                            aria-labelledby="list-subheader"
+                                            subheader={
+                                                <ListSubheader
+                                                    component="div"
+                                                    id="list-subheader"
+                                                >
+                                                    Job Details
+                                                </ListSubheader>
+                                            }
+                                        >
+                                            <JobDetailsListItem
+                                                title="Job Type"
+                                                subtitle="Microtask"
+                                            />
+                                            <JobDetailsListItem
+                                                title="Available slots"
+                                                subtitle="1"
+                                            />
+                                            <JobDetailsListItem
+                                                title="Payment"
+                                                subtitle="GCash"
+                                            />
+                                            <JobDetailsListItem
+                                                title="Job Posted"
+                                                subtitle="3 days ago"
+                                            />
+                                        </List>
+                                    </Card>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Card color="primary" variant="outlined">
+                                        <List
+                                            dense={true}
+                                            sx={{
+                                                width: "100%",
+                                                bgcolor: "background.paper",
+                                            }}
+                                            aria-labelledby="list-subheader"
+                                            subheader={
+                                                <ListSubheader
+                                                    component="div"
+                                                    id="list-subheader"
+                                                >
+                                                    Interested Freelancers
+                                                </ListSubheader>
+                                            }
+                                        >
+                                            <ListItem alignItems="flex-start">
+                                                <ListItemAvatar>
+                                                    <Avatar
+                                                        alt="Sebastian Curtis Lavarias"
+                                                        src="/static/images/avatar/1.jpg"
+                                                    />
+                                                </ListItemAvatar>
+                                                <ListItemText
+                                                    primary="Sebastian Curtis Lavarias"
+                                                    secondary="Hi! This is Sebastian a world class freelancer."
+                                                />
+                                            </ListItem>
+                                        </List>
+                                    </Card>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Box>
--- resources/js/Components/JobPostCard.jsx
+++ resources/js/Components/JobPostCard.jsx
@@ -21,127 +21,137 @@ export default function JobPostCard() {
     const theme = useTheme();
 
     const onHandleModalOpen = () => setIsModalOpen(true);
+    const ontest = (value) => {
+        console.log(value);
+        setIsModalOpen(false);
+    };
 
     return (
-        <Box sx={{ width: "100%" }} onClick={onHandleModalOpen} ref={rootRef}>
-            <Card color="primary" variant="outlined">
-                <ListItem
-                    dense={true}
-                    alignItems="flex-start"
-                    secondaryAction={
-                        <React.Fragment>
-                            <Chip size="small" label="Microtask" />
-                        </React.Fragment>
-                    }
-                >
-                    <ListItemAvatar>
-                        <Avatar
-                            alt="Remy Sharp"
-                            src="https://avatars.githubusercontent.com/u/24890911?v=4"
-                        />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={
+        <React.Fragment>
+            <Box
+                sx={{ width: "100%" }}
+                onClick={onHandleModalOpen}
+                ref={rootRef}
+            >
+                <Card color="primary" variant="outlined">
+                    <ListItem
+                        dense={true}
+                        alignItems="flex-start"
+                        secondaryAction={
                             <React.Fragment>
-                                <Typography
-                                    sx={{ fontWeight: 500 }}
-                                    component="h6"
-                                    variant="subtitle1"
-                                >
-                                    John Doe
-                                </Typography>
+                                <Chip size="small" label="Microtask" />
                             </React.Fragment>
                         }
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    component="span"
-                                    variant="body2"
-                                    sx={{ fontSize: ".8rem" }}
-                                >
-                                    3 days ago
-                                </Typography>
-                            </React.Fragment>
-                        }
-                    />
-                </ListItem>
-                <CardContent sx={{ paddingTop: "0" }}>
-                    <Typography
-                        variant="body1"
-                        component="h6"
-                        sx={{ fontWeight: 500 }}
                     >
-                        Encode human resource data
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        component="p"
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="https://avatars.githubusercontent.com/u/24890911?v=4"
+                            />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{ fontWeight: 500 }}
+                                        component="h6"
+                                        variant="subtitle1"
+                                    >
+                                        John Doe
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        component="span"
+                                        variant="body2"
+                                        sx={{ fontSize: ".8rem" }}
+                                    >
+                                        3 days ago
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                        />
+                    </ListItem>
+                    <CardContent sx={{ paddingTop: "0" }}>
+                        <Typography
+                            variant="body1"
+                            component="h6"
+                            sx={{ fontWeight: 500 }}
+                        >
+                            Encode human resource data
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            component="p"
+                            sx={{
+                                paddingBottom: ".5rem",
+                            }}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Eum, sint... View job
+                        </Typography>
+                        <Typography variant="caption" component="span">
+                            3 available slots | GCash
+                        </Typography>
+                    </CardContent>
+                    <CardActions
                         sx={{
-                            paddingBottom: ".5rem",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
                         }}
                     >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Eum, sint... View job
-                    </Typography>
-                    <Typography variant="caption" component="span">
-                        3 available slots | GCash
-                    </Typography>
-                </CardContent>
-                <CardActions
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <Box>
-                        <AvatarGroup max={6}>
-                            <Avatar
-                                alt="Remy Sharp"
-                                src="https://avatars.githubusercontent.com/u/24890911?v=4"
+                        <Box>
+                            <AvatarGroup max={6}>
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="https://avatars.githubusercontent.com/u/24890911?v=4"
+                                    sx={{
+                                        width: "1.5rem",
+                                        height: "1.5rem",
+                                    }}
+                                />
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="https://avatars.githubusercontent.com/u/24890911?v=4"
+                                    sx={{
+                                        width: "1.5rem",
+                                        height: "1.5rem",
+                                    }}
+                                />
+                                <Avatar
+                                    alt="Remy Sharp"
+                                    src="https://avatars.githubusercontent.com/u/24890911?v=4"
+                                    sx={{
+                                        width: "1.5rem",
+                                        height: "1.5rem",
+                                    }}
+                                />
+                            </AvatarGroup>
+                        </Box>
+                        <Box>
+                            <Typography
                                 sx={{
-                                    width: "1.5rem",
-                                    height: "1.5rem",
+                                    fontWeight: "bold",
+                                    color: theme.palette.success.main,
                                 }}
-                            />
-                            <Avatar
-                                alt="Remy Sharp"
-                                src="https://avatars.githubusercontent.com/u/24890911?v=4"
-                                sx={{
-                                    width: "1.5rem",
-                                    height: "1.5rem",
-                                }}
-                            />
-                            <Avatar
-                                alt="Remy Sharp"
-                                src="https://avatars.githubusercontent.com/u/24890911?v=4"
-                                sx={{
-                                    width: "1.5rem",
-                                    height: "1.5rem",
-                                }}
-                            />
-                        </AvatarGroup>
-                    </Box>
-                    <Box>
-                        <Typography
-                            sx={{
-                                fontWeight: "bold",
-                                color: theme.palette.success.main,
-                            }}
-                            variant="subtitle2"
-                            component="p"
-                        >
-                            ₱ 150.00
-                        </Typography>
-                    </Box>
-                </CardActions>
-            </Card>
+                                variant="subtitle2"
+                                component="p"
+                            >
+                                ₱ 150.00
+                            </Typography>
+                        </Box>
+                    </CardActions>
+                </Card>
+            </Box>
 
             <JobInformationModal
                 open={isModalOpen}
                 rootRef={rootRef}
                 setIsModalOpen={setIsModalOpen}
             />
-        </Box>
+        </React.Fragment>
     );
 }
